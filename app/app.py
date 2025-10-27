40b688b0c9068470c76761f10ae5e313e57132fc
sirishavadlamudi <vsirisha028@gmail.com>
2025-10-27 18:49:54 +0530

    Add FastAPI app and Dockerfile

--- app/app.py
+++ app/app.py
@@ -1,21 +1,8 @@
-from flask import Flask
-import os
+from fastapi import FastAPI
 import psycopg2
 
-app = Flask(__name__)
+app = FastAPI()
 
-DB_HOST = os.environ.get("POSTGRES_HOST", "postgres")
-DB_NAME = os.environ.get("POSTGRES_DB", "pgdb")
-DB_USER = os.environ.get("POSTGRES_USER", "pguser")
-DB_PASS = os.environ.get("POSTGRES_PASSWORD", "pgpassword")
-
-@app.route("/")
-def index():
-    try:
-        conn = psycopg2.connect(
-            host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASS, connect_timeout=3
-        )
-        conn.close()
-        return "OK, DB reachable", 200
-    except Exception as e:
-        return f"DB not reachable: {e}", 500
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI + PostgreSQL on GKE!"}
